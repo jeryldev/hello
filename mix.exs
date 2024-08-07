81150c48aaa85495c76c53272d2863bf9080fcf3
jeryldev <jeryldev@gmail.com>
2024-08-07 12:06:55 +0800

    Add tigerbeetlex and run mix deps.get && mix deps.compile && mix ecto.setup && mix test;

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule Hello.MixProject do
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+      {:tigerbeetlex, github: "rbino/tigerbeetlex", submodules: true}
     ]
   end
 
